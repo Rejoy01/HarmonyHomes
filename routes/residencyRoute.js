9d004e00e8624bb351969f6130819de01769b3f3
Rejoy01 <rejoywilson65@gmail.com>
2023-10-04 02:29:32 +0530

    residency routes

--- routes/residencyRoute.js
+++ routes/residencyRoute.js
@@ -1,7 +1,8 @@
 import express from "express"
-import { createResidency } from "../controllers/ResidencyController.js";
+import { createResidency, getAllResidencies, getResidency } from "../controllers/ResidencyController.js";
 const router = express.Router();
 
 router.post('/create',createResidency)
-
+router.get('/allres',getAllResidencies)
+router.get('/:id',getResidency)
 export {router as residencyRoute}
